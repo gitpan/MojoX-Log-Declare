b04c742d0f03b278376798babb5a4fb4fcb0c943
Companies House <chgovuk@cpan.org>
2014-01-23 11:07:49 +0000

    Import of CHGOVUK/MojoX-Log-Declare-0.02 from CPAN.
    gitpan-cpan-distribution: MojoX-Log-Declare
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CHGOVUK/MojoX-Log-Declare-0.02.tar.gz
    gitpan-cpan-author:       CHGOVUK
    gitpan-cpan-maturity:     released

---
+++ lib/MojoX/Log/Declare.pm
@@ -0,0 +1,42 @@
+package MojoX::Log::Declare;
+
+use Mojo::Base -base;
+use Log::Declare;
+
+our $VERSION = '0.02';
+
+use strict;
+use warnings;
+
+{
+    no strict 'refs';
+    no warnings;
+
+    for my $level (@Log::Declare::level_priority) {
+
+        *{ __PACKAGE__ . "::$level" } = sub {
+            my $self = shift;
+            Log::Declare->log( $level, ['MOJO'], @_ );
+        };
+    }
+}
+
+1;
+
+=head1 NAME
+
+MojoX::Log::Declare - Integrate Log::Declare with Mojolicious
+
+=head1 SYNOPSIS
+
+  use Mojo::Base 'Mojolicious';
+  use MojoX::Log::Declare;
+
+  sub startup {
+     my $self = shift;
+
+     $self->log( MojoX::Log::Declare->new() );
+     # ...
+  }
+
+=cut
